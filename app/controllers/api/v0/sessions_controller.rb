07c2b525a64f320bd2646a386790eeb52e6c923d
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-10 17:15:07 -0600

    Feat: sad path testing working

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,9 +1,16 @@
 class Api::V0::SessionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_invalid_response
   def create
     @user = User.find_by!(email: params[:session][:email])
 
-    if @user.authenticate(params[:session][:password])
+    if @user.authenticate(params[:session][:password]) && @user.authenticate(params[:session][:password_confirmation])
       render json: UsersSerializer.new(@user), status: 201
+    else
+      render_invalid_response
     end
+  end
+
+  def render_invalid_response
+    render json: { error: "Invalid credentials" }, status: 400
   end
 end
