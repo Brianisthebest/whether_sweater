1a001b3c0acc06649778cebe798845205b8f7759
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-10 15:55:11 -0600

    feat: update validations for api key, update create action in user controller, create users serializer

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    require 'pry'; binding.pry
+    @user = User.new(user_params)
+    @user.api_key = SecureRandom.hex
+
+    if @user.save
+      render json: UsersSerializer.new(@user), status: 201
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
