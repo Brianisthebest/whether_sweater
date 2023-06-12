a624ebbc7904754314ba3c73ea06da6a8456e4f3
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-12 11:35:47 -0600

    Feat: sad path for integer less that one for quantity covered

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::BooksController < ApplicationController
   def index
     if params[:location].empty?
       render json: { error: 'Please provide a location' }, status: 400
+    elsif params[:quantity].to_i < 1
+      render json: { error: 'Please provide a valid quantity' }, status: 400
     else
       book_search = BookSearchFacade.new.get_books(params[:location], params[:quantity])
       render json: BooksSerializer.new(book_search)
