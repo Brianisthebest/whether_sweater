a26664c9c04a11dc3937dc26c1e28361bfbf1dbb
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-12 11:11:52 -0600

    Refactor: sad path working for no location provided

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    book_search = BookSearchFacade.new.get_books(params[:location], params[:quantity])
-
-    render json: BooksSerializer.new(book_search)
+    if params[:location].empty?
+      render json: { error: 'Please provide a location' }, status: 400
+    else
+      book_search = BookSearchFacade.new.get_books(params[:location], params[:quantity])
+      render json: BooksSerializer.new(book_search)
+    end
   end
 end
