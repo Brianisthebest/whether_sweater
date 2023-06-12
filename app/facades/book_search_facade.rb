ed44814381402dbfc43a4a604bb07a6d478c3ef1
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-06-12 10:26:49 -0600

    WIP: continue to work on book search facade

---
+++ app/facades/book_search_facade.rb
@@ -0,0 +1,29 @@
+class BookSearchFacade
+  def get_books(location, quantity)
+    books = BookSearchService.new.get_books(location, quantity)
+
+    mapquest = MapquestService.new.get_lat_lng(location)
+    lat = mapquest[:results][0][:locations][0][:latLng][:lat]
+    lng = mapquest[:results][0][:locations][0][:latLng][:lng]
+
+    weather = WeatherService.new.get_weather(lat, lng)
+
+    books_hash = books[:docs].map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title],
+        publisher: book[:publisher]
+      }
+    end
+
+    books = {destination: location,
+      forecast: { summary: weather[:current][:condition][:text],
+                    temperature: weather[:current][:temp_f]
+                   },
+      total_books_found: books[:numFound],
+      books: books_hash
+     }
+
+    Book.new(books)
+  end
+end
